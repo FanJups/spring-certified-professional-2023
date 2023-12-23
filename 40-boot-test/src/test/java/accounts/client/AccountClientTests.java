ddb83b123a7793b77748479469d80d59d698997b
Fanon Jupkwo <jupsfan@gmail.com>
2023-12-23 01:37:03 +0000

    Spring Boot - Module 6 - Spring Boot Testing

--- 40-boot-test/src/test/java/accounts/client/AccountClientTests.java
+++ 40-boot-test/src/test/java/accounts/client/AccountClientTests.java
@@ -2,7 +2,11 @@ package accounts.client;
 
 import common.money.Percentage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import rewards.internal.account.Account;
@@ -27,10 +31,13 @@ import static org.junit.jupiter.api.Assertions.*;
 // TODO-01: Make this class a Spring Boot test class
 // - Add @SpringBootTest annotation with WebEnvironment.RANDOM_PORT
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AccountClientTests {
 
 	// TODO-02: Autowire TestRestTemplate bean to a field
 	// - Name the field as restTemplate
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	// TODO-03: Update code below to use TestRestTemplate (as opposed to RestTemplate)
 	// - Remove RestTemplate from this code
@@ -44,9 +51,8 @@ public class AccountClientTests {
 	 * server URL ending with the servlet mapping on which the application can be
 	 * reached.
 	 */
-	private static final String BASE_URL = "http://localhost:8080";
+	private static final String BASE_URL = "";
 
-	private RestTemplate restTemplate = new RestTemplate();
 	private Random random = new Random();
 
 	@Test
@@ -108,11 +114,9 @@ public class AccountClientTests {
 
 		restTemplate.delete(newBeneficiaryLocation);
 
-		HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
-			System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
-			restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
-		});
-		assertThat(httpClientErrorException.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		ResponseEntity<Beneficiary> response = restTemplate.getForEntity(newBeneficiaryLocation, Beneficiary.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 	// TODO-05: Observe a log message in the console indicating
--- 40-boot-test/src/test/java/accounts/web/AccountControllerBootTests.java
+++ 40-boot-test/src/test/java/accounts/web/AccountControllerBootTests.java
@@ -1,43 +1,53 @@
 package accounts.web;
 
+import accounts.AccountManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import rewards.internal.account.Account;
 
 // TODO-06: Get yourself familiarized with various testing utility classes
 // - Uncomment the import statements below
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.BDDMockito.*;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // TODO-07: Replace @ExtendWith(SpringExtension.class) with the following annotation
-// - @WebMvcTest(AccountController.class) // includes @ExtendWith(SpringExtension.class)
-@ExtendWith(SpringExtension.class)
+@WebMvcTest(AccountController.class) // includes @ExtendWith(SpringExtension.class)
 public class AccountControllerBootTests {
 
 	// TODO-08: Autowire MockMvc bean
+	@Autowired
+	private MockMvc mockMvc;
 
 	// TODO-09: Create AccountManager mock bean using @MockBean annotation
+	@MockBean
+	private AccountManager accountManager;
 
 	// TODO-10: Write positive test for GET request for an account
 	// - Uncomment the code and run the test and verify it succeeds
 	@Test
 	public void accountDetails() throws Exception {
 
-		//given(accountManager.getAccount(0L))
-		//		.willReturn(new Account("1234567890", "John Doe"));
+		given(accountManager.getAccount(0L))
+				.willReturn(new Account("1234567890", "John Doe"));
 
-		//mockMvc.perform(get("/accounts/0"))
-		//	   .andExpect(status().isOk())
-		//	   .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-		//	   .andExpect(jsonPath("name").value("John Doe"))
-		//	   .andExpect(jsonPath("number").value("1234567890"));
+		mockMvc.perform(get("/accounts/0"))
+			   .andExpect(status().isOk())
+			   .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			   .andExpect(jsonPath("name").value("John Doe"))
+			   .andExpect(jsonPath("number").value("1234567890"));
 
-		//verify(accountManager).getAccount(0L);
-
+		verify(accountManager).getAccount(0L);
 	}
 
 	// TODO-11: Write negative test for GET request for a non-existent account
@@ -47,15 +57,17 @@ public class AccountControllerBootTests {
 	@Test
 	public void accountDetailsFail() throws Exception {
 
-		//given(accountManager.getAccount(any(Long.class)))
-		//		.willThrow(new IllegalArgumentException("No such account with id " + 0L));
+		given(accountManager.getAccount(any(Long.class)))
+				.willThrow(new IllegalArgumentException("No such account with id " + 0L));
 
 		// (Write code here)
 		// - Use mockMvc to perform HTTP Get operation using "/accounts/9999"
         //   as a non-existent account URL
 		// - Verify that the HTTP response status is 404
+		mockMvc.perform(get("/accounts/9999"))
+				.andExpect(status().isNotFound());
 
-		//verify(accountManager).getAccount(any(Long.class));
+		verify(accountManager).getAccount(any(Long.class));
 
 	}
 
@@ -66,11 +78,11 @@ public class AccountControllerBootTests {
 	@Test
 	public void createAccount() throws Exception {
 
-		//Account testAccount = new Account("1234512345", "Mary Jones");
-		//testAccount.setEntityId(21L);
+		Account testAccount = new Account("1234512345", "Mary Jones");
+		testAccount.setEntityId(21L);
 
-		//given(accountManager.save(any(Account.class)))
-		//		.willReturn(testAccount);
+		given(accountManager.save(any(Account.class)))
+				.willReturn(testAccount);
 
 		// (Write code here)
 		// Use mockMvc to perform HTTP Post operation to "/accounts"
@@ -80,6 +92,11 @@ public class AccountControllerBootTests {
 		//   object into Json string)
 		// - Verify that the response status is 201
 		// - Verify that the response "Location" header contains "http://localhost/accounts/21"
+		mockMvc.perform(post("/accounts")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(asJsonString(testAccount)))
+				.andExpect(status().isCreated())
+				.andExpect(header().string("Location","http://localhost/accounts/21"));
 
 		//verify(accountManager).save(any(Account.class));
 
